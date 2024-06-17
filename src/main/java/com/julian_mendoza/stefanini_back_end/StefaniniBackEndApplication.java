2b30b28e56191400c429176c784bc9bcf9d7b072
Julian Mendoza <jdmendoza_90@hotmail.com>
2024-06-16 22:11:08 -0500

    First commit

---
+++ src/main/java/com/julian_mendoza/stefanini_back_end/StefaniniBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.julian_mendoza.stefanini_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StefaniniBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StefaniniBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julian_mendoza/stefanini_back_end/StefaniniBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julian_mendoza.stefanini_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StefaniniBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
